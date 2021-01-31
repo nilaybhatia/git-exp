92189aadc90752ab04f3c2dea068b69084a845db
nilaybhatia <bhatianilay@gmail.com>
2021-01-31 18:17:25 +0530

    Basic C++ file for git bisect, compiles

---
+++ bisect.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // https://git-scm.com/docs/git-bisect
+    cout << "C++ file for testing git bisect";
+    /*
+    Basic file for 1st commit, compiles
+    */
+    return 0;
+}
