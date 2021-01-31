5ee396b094c80e87c3515f0ea6386ff470221c80
nilaybhatia <bhatianilay@gmail.com>
2021-01-31 18:20:38 +0530

    Some additions from master, compiles

--- bisect.cpp
+++ bisect.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 int main(){
     // https://git-scm.com/docs/git-bisect
-    cout << "C++ file for testing git bisect";
+    cout << "C++ file for testing git bisect\n";
     /*
     Basic file for 1st commit, compiles
+    */
+    cout << "Some more additions from master, compiles";
+    /*
+    Now will commit the above line and checkout a new dev branch
     */
     return 0;
 }
